5f777783e5675191ad53c3e110db3f0b4f6daf15
Samir Tamang <samirllama@gmail.com>
2025-09-10 09:20:27 -0500

    feat: added function to read file content

--- tests.py
+++ tests.py
@@ -1,45 +1,30 @@
 import unittest
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
-BASE_DIR = "."
-
-class TestGetFilesInfo(unittest.TestCase):
+class TestGetFilesContent(unittest.TestCase):
     def test_calculator_directory(self):
-        result = get_files_info("calculator", BASE_DIR)
-
-        self.assertIn('- tests.py', result)
-        self.assertIn('- main.py', result)
-        self.assertIn('- pkg', result)
-
-        self.assertIn('file_size=', result)
-        self.assertIn('is_dir=', result)
+        result = get_file_content("calculator", "main.py")
 
         print('Result for current directory:')
         print(result)
 
     def test_pkg_directory(self):
-        result = get_files_info("calculator", "pkg")
+        result = get_file_content("calculator", "pkg/calculator.py")
 
-        self.assertIn('- calculator.py', result)
-        self.assertIn('- render.py', result)
-
-        self.assertIn('file_size=', result)
-        self.assertIn('is_dir=', result)
-
-        print(f"Result for 'pkg' directory:")
+        print(f"Result for 'pkg/calculator.py':")
         print(result)
 
     def test_bin_directory(self):
-        result = get_files_info("calculator", "/bin")
+        result = get_file_content("calculator", "/bin/cat")
 
-        print(f"Result for '/bin' directory:")
+        print(f"Result for '/bin/cat' directory:")
         print(result)
 
     def test_path_traversal_attempt_is_denied(self):
-        result = get_files_info("calculator", "../")
+        result = get_file_content("calculator", "pkg/does_not_exist.py")
 
-        print(f"Result for '../' directory:")
+        print(f"Result for 'pkg/does_not_exist.py':")
         print(result)
 
 
