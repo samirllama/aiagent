9fa15ae518d8aa417f2fcb3fe9f0236f0cd6c26a
Samir Tamang <samirllama@gmail.com>
2025-09-10 10:33:56 -0500

    feat: added ability to write and overwrite files

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,39 +1,21 @@
 import os
-from typing import Optional
-
-def count_file_chars(path: str, encoding= 'utf-8')-> Optional[int]:
-    try:
-        with open(path,'r', encoding=encoding) as f:
-            content = f.read()
-            return len(content)
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        return None
-    except FileNotFoundError:
-        print(f"File path {path} not found")
-        return None
+from config import MAX_CHARS
 
 
 def get_file_content(working_directory:str, file_path:str) -> str:
     abs_working_dir = os.path.abspath(working_directory)
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
-
     if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
-
     try:
-        with open(abs_file_path,'r', encoding='utf-8') as f:
-            content = f.read()
-            if len(content) > 10000:
-                truncated_content = content[:10000]
-                message = f'[...File "{file_path}" truncated at 10000 characters]'
-                return truncated_content + message
-            else:
-                return content
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
     except Exception as e:
         return f'Error reading file "{file_path}": {e}'
-    except FileNotFoundError:
-        return f"File path {abs_file_path} not found"
-
--- tests.py
+++ tests.py
@@ -1,32 +1,15 @@
-import unittest
-from functions.get_file_content import get_file_content
+from functions.write_file_content import write_file
 
+def test():
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print(result)
 
-class TestGetFilesContent(unittest.TestCase):
-    def test_calculator_directory(self):
-        result = get_file_content("calculator", "main.py")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print(result)
 
-        print('Result for current directory:')
-        print(result)
-
-    def test_pkg_directory(self):
-        result = get_file_content("calculator", "pkg/calculator.py")
-
-        print(f"Result for 'pkg/calculator.py':")
-        print(result)
-
-    def test_bin_directory(self):
-        result = get_file_content("calculator", "/bin/cat")
-
-        print(f"Result for '/bin/cat' directory:")
-        print(result)
-
-    def test_path_traversal_attempt_is_denied(self):
-        result = get_file_content("calculator", "pkg/does_not_exist.py")
-
-        print(f"Result for 'pkg/does_not_exist.py':")
-        print(result)
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print(result)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    test()
